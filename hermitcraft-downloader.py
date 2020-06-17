32a46994ec654271bd0ed1c010429eeb3fe7d7f6
Matteus Magnusson <senth.wallace@gmail.com>
2020-06-17 19:01:30 +0200

    ✨ Now shows title and saves as an episode
    Changed from YYYY-MM-DD to s01eXX formatting as that works better.
    The title of the episode is saved in the filename.
    Add title metadata to the mp4 file.

--- hermitcraft-downloader.py
+++ hermitcraft-downloader.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+import argparse
 import requests
 import re
+import sqlite3
 from pathlib import Path
 from subprocess import run
 from os import path, makedirs, remove
@@ -11,6 +13,71 @@ from datetime import datetime, timedelta
 
 MINECRAFT_DIR = '/mnt/lvm/series/Minecraft'
 MAX_DAYS_BACK = 1
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--verbose', action='store_true',
+                    help='Prints out helpful debug messages.')
+parser.add_argument('-p', '--pretend', action='store_true',
+                    help='Only pretent to download, convert, and store files')
+args = parser.parse_args()
+
+
+def log_message(message):
+    if args.verbose:
+        print(message)
+
+
+class Model:
+    FILE_PATH = path.expanduser('~/.youtube-series.downloader.db')
+    FILE = Path(FILE_PATH)
+
+    def __init__(self):
+        log_message('Sqlite DB location: {}'.format(Model.FILE_PATH))
+        self.connection = sqlite3.connect(Model.FILE_PATH)
+        self.cursor = self.connection.cursor()
+
+        # Create DB (if not exists)
+        self._create_db()
+
+    def _create_db(self):
+        self.connection.execute(
+            'Create TABLE IF NOT EXISTS video (id TEXT, episode_number INTEGER, channel_name TEXT)')
+        self.connection.commit()
+
+    def add_downloaded(self, channel_name, video_id):
+        episode_number = self.get_next_episode_number(channel_name)
+
+        log_message("{} Add to downloaded with episode number {}.".format(
+            video_id, episode_number))
+
+        if not args.pretend:
+            sql = "INSERT INTO video (id, episode_number, channel_name) VALUES(?, ?, ?)"
+            self.connection.execute(
+                sql, (video_id, episode_number, channel_name))
+            self.connection.commit()
+
+    def get_next_episode_number(self, channel_name):
+        sql_get_latest_episode = "SELECT episode_number FROM video WHERE channel_name=? ORDER BY episode_number DESC"
+        self.cursor.execute(sql_get_latest_episode, [channel_name])
+        row = self.cursor.fetchone()
+        if row:
+            return row[0] + 1
+        else:
+            return 1
+
+    def has_downloaded(self, video_id):
+        sql = "SELECT episode_number FROM video WHERE id=?"
+        self.cursor.execute(sql, [video_id])
+        row = self.cursor.fetchone()
+        return bool(row)
+
+
+class Video:
+    def __init__(self, id, date, title, channel_name):
+        self.id = id
+        self.date = date
+        self.title = title
+        self.channel_name = channel_name
 
 
 class Channel:
@@ -23,97 +90,126 @@ class Channel:
         self.channel_id = channel_id
         self.filters = filters
 
-    def get_video_ids(self):
+    def get_videos(self):
+        log_message('\n\nGetting videos from ' + self.name)
         url = Channel.RSS_PREFIX + self.channel_id
         xml = requests.get(url).text
 
         matches = Channel.REGEX.findall(xml)
 
-        video_ids = []
+        videos = []
 
         for groups in matches:
             id = groups[0]
             title = groups[1]
             date = groups[2]
+            log_message('{}: Checking video ({})'.format(id, date))
 
             if not self._matches_filter(title) and self._is_new(date):
-                video_ids.append(id)
+                video = Video(id, date, title, self.name)
+                log_message('{}: Appending video: {}'.format(id, title))
+                videos.append(video)
 
-        return video_ids
+        return videos
 
     def _is_new(self, dateString):
         date = datetime.strptime(dateString, '%Y-%m-%dT%H:%M:%S%z')
         diff_time = datetime.now() - date.replace(tzinfo=None)
 
         if diff_time.days <= MAX_DAYS_BACK:
+            log_message('Is new by {} days'.format(diff_time.days))
             return True
         else:
+            log_message('is old by {} days'.format(diff_time.days))
             return False
 
     def _matches_filter(self, title):
         for filter in self.filters:
-            if re.match(filter, title):
+            if re.search(filter, title):
+                log_message('({}) Matched filter: {}'.format(title, filter))
                 return True
         return False
 
 
 class Downloader:
-    TMP_DOWNLOAD = '/tmp/%(upload_date)s - %(id)s.%(ext)s'
+    TMP_DOWNLOAD = '/tmp/downloaded.mkv'
+    TMP_CONVERTED = '/tmp/converted.mp4'
 
-    def __init__(self, channel_name, video_id):
-        self.channel_name = channel_name
-        self.video_id = video_id
+    def __init__(self, db, video):
+        self.db = db
+        self.video = video
 
     def has_downloaded(self):
-        if any(Path(MINECRAFT_DIR).rglob('*' + self.video_id + '*')):
+        # REMOVE remove after a few days
+        if any(Path(MINECRAFT_DIR).rglob('*' + self.video.id + '*')):
             return True
-        return False
+        return self.db.has_downloaded(self.video.id)
 
     def download(self):
-        run(['youtube-dl', '-o', Downloader.TMP_DOWNLOAD, '-f', 'bestvideo[height>=1080,fps=60]+bestaudio',
-             '--merge-output-format', 'mkv', '--restrict-filenames', self.video_id])
-
+        if not args.pretend:
+            run(['youtube-dl', '-o', Downloader.TMP_DOWNLOAD, '-f', 'bestvideo[height>=1080,fps=60]+bestaudio',
+                 '--merge-output-format', 'mkv', '--restrict-filenames', '--', self.video.id])
         self._convert()
 
-        print(str(self._get_in_and_out_filepath()))
+        print(str(self._get_out_filepath()))
 
     def _convert(self):
         self._create_out_dir()
-        in_filepath, out_filepath = self._get_in_and_out_filepath()
-        out_path_temp = '/tmp/out.mkv'
+        out_filepath = self._get_out_filepath()
+        downloaded = False
 
-        run(['ffmpeg', '-i', in_filepath, '-threads', '2', '-filter_complex',
-             '[0:v]setpts=(2/3)*PTS[v];[0:a]atempo=1.5[a]', '-map', '[v]', '-map', '[a]', out_path_temp])
+        if not args.pretend:
+            completed_process = run(['ffmpeg', '-i', Downloader.TMP_DOWNLOAD, '-metadata', 'title="{}"'.format(self.video.title), '-threads', '2', '-filter_complex',
+                                     '[0:v]setpts=(2/3)*PTS[v];[0:a]atempo=1.5[a]', '-map', '[v]', '-map', '[a]', Downloader.TMP_CONVERTED])
 
-        # Copy the temprory file to series/Minecraft
-        copyfile(out_path_temp, out_filepath)
+            if completed_process.returncode == 0:
+                downloaded = True
+                # Copy the temprory file to series/Minecraft
+                log_message('{} Copying file to {}'.format(
+                    self.video.id, out_filepath))
+                copyfile(Downloader.TMP_CONVERTED, out_filepath)
 
-        # Delete temporary files original file
-        remove(out_path_temp)
-        remove(in_filepath)
+                # Delete temporary files original file
+                log_message(
+                    '{} Deleting temporary files'.format(self.video.id))
+                remove(Downloader.TMP_CONVERTED)
+                remove(Downloader.TMP_DOWNLOAD)
+
+        if downloaded or args.pretend:
+            self.db.add_downloaded(self.video.channel_name, self.video.id)
 
     def _get_out_dir(self):
-        return path.join(MINECRAFT_DIR, self.channel_name, 'Season 01')
+        return path.join(MINECRAFT_DIR, self.video.channel_name, 'Season 01')
 
     def _create_out_dir(self):
         makedirs(self._get_out_dir(), exist_ok=True)
 
-    def _get_in_and_out_filepath(self):
-        in_filename = ''
-        for file in Path('/tmp').rglob('*' + self.video_id + '*'):
-            in_filename = file.name
-        in_filepath = path.join('/tmp', in_filename)
+    def _get_filename_safe(self):
+        # Replace : or | with -
+        title = re.sub(r'[:\|]', ' -', self.video.title)
 
-        out_filename = self.channel_name + ' - ' + re.sub(
-            '(\d{4})(\d{2})(\d{2}) ', '\g<1>-\g<2>-\g<3> ', in_filename)
+        # Remove illegal characters
+        title = re.sub(r'[^\w\ \-\.,]', '', title)
+
+        # Remove all places where there are two whitespaces
+        title = ' '.join(title.split())
+
+        return title
+
+    def _get_out_filepath(self):
+        date_string = self.video.date[:10]
+        episode_number = self.db.get_next_episode_number(
+            self.video.channel_name)
+        out_filename = '{} - s01e{} - {}.mp4'.format(
+            self.video.channel_name, episode_number, self._get_filename_safe())
         out_filepath = path.join(self._get_out_dir(), out_filename)
 
-        return in_filepath, out_filepath
+        return out_filepath
 
 
 channels = [
     Channel('Cubfan', 'UC9lJXqw4QZw-HWaZH6sN-xw'),
-    Channel('Xisuma', 'UCU9pX8hKcrx06XfOB-VQLdw', ['Live Now', 'LIVE NOW']),
+    Channel('Xisuma', 'UCU9pX8hKcrx06XfOB-VQLdw', [r'Live Now', r'LIVE NOW']),
     Channel('Keralis', 'UCcJgOennb0II4a_qi9OMkRA'),
     Channel('Etho', 'UCFKDEp9si4RmHFWJW1vYsMA'),
     Channel('Iskall', 'UCZ9x-z3iOnIbJxVpm1rsu2A'),
@@ -121,26 +217,33 @@ channels = [
     Channel('Grian', 'UCR9Gcq0CMm6YgTzsDxAxjOQ'),
     Channel('False Symmetry', 'UCuQYHhF6on6EXXO-_i_ClHQ'),
     Channel('ImpulseSV', 'UCuMJPFqazQI4SofSFEd-5zA'),
-    Channel('Hypnotizd', 'UChi5MyXJLQuPni3dM19Ar3g', ['Modded']),
+    Channel('Hypnotizd', 'UChi5MyXJLQuPni3dM19Ar3g', [r'Modded']),
     Channel('Tango Tek', 'UC4YUKOBld2PoOLzk0YZ80lw'),
     Channel('Good Times With Scar', 'UCodkNmk9oWRTIYZdr_HuSlg'),
-    Channel('Mumbo Jumbo', 'UChFur_NwVSbUozOcF_F2kMg', ['^(?!Hermitcraft).*']),
+    Channel('Mumbo Jumbo', 'UChFur_NwVSbUozOcF_F2kMg',
+            [r'^(?!Hermitcraft).*']),
     Channel('BDoubleO', 'UClu2e7S8atp6tG2galK9hgg'),
     Channel('Zombie Cleo', 'UCjI5qxhtyv3srhWr60HemRw'),
     Channel('Docm', 'UC4O9HKe9Jt5yAhKuNv3LXpQ'),
     Channel('Ilmango', 'UCHSI8erNrN6hs3sUK6oONLA'),
     Channel('Gnembon', 'UCRtyLX-ej-H1PSiaw8g9aIA'),
+    Channel('Rendog', 'UCDpdtiUfcdUCzokpRWORRqA'),
+    Channel('Stressmonster', 'UC24lkOxZYna9nlXYBcJ9B8Q'),
 ]
 
+db = Model()
+total_downloaded = 0
 
 for channel in channels:
-    video_ids = channel.get_video_ids()
+    videos = channel.get_videos()
 
-    for video_id in video_ids:
-        print('Video id: ' + video_id)
-        downloader = Downloader(channel.name, video_id)
+    for video in videos:
+        downloader = Downloader(db, video)
 
         if not downloader.has_downloaded():
             downloader.download()
+            total_downloaded += 1
         else:
-            print('Skipping, already downloaded video')
+            log_message('Skipping, already downloaded video')
+
+log_message('Downloaded {} episodes'.format(total_downloaded))
