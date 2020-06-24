a007e938cb52829e0206de347da82a1f830f6137
Matteus Magnusson <senth.wallace@gmail.com>
2020-06-24 17:25:28 +0200

    ✨ Set video title for mp4 files in folder

---
+++ set-video-title.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from pathlib import Path
+from os import remove
+from shutil import copy2
+from subprocess import run
+import re
+
+# Get all mp4 files in the current directory
+files = Path('.').glob('*.mp4')
+tmp_file = '/tmp/metadata-title.tmp.mp4'
+
+for file in files:
+    title = re.sub(r'^.*s\d{2}e\d+ - ', '', str(file))
+    title = title.replace('.mp4', '')
+    print('Title: {}, file: {}'.format(title, file))
+    run(['ffmpeg', '-i', str(file), '-c', 'copy',
+         '-metadata', 'title={}'.format(title), tmp_file])
+
+    if Path(tmp_file).exists():
+        remove(str(file))
+        copy2(tmp_file, str(file))
+        remove(tmp_file)
